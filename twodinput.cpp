ba4a60c1a167666953441805802c99d34d5afd3f
theoriginalshyam <148356731+theoriginalshyam@users.noreply.github.com>
2024-01-27 17:12:23 +0530

    Create twodinput.cpp

---
+++ twodinput.cpp
@@ -0,0 +1,13 @@
+ll n; 
+cin>>n;
+vector<vi> v(n);
+ll k;
+vl size(0);
+ll split = 0, comb = 0;
+for(int i =0; i < n; i++){
+    cin>>k;
+    size.push_back(k);
+    v[i].resize(k);
+    for(int j = 0; j < k; j++)
+    cin>>v[i][j];
+}
